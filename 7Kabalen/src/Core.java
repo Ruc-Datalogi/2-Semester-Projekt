dc4b116014ccfc25cb93ed9ced9f58a96fe0c207
MikkelEsb <mieles@ruc.dk>
2020-03-05 20:58:54 +0100

    Testing processing library

--- 7Kabalen/src/Core.java
+++ 7Kabalen/src/Core.java
@@ -1,9 +1,21 @@
 import processing.core.*;
 
-public class Core {
+public class Core extends PApplet{
     /*
     Core should initialize and manage the program
 
      */
+    public static void main(String[] args) {
+        PApplet.main("Core");
+    }
+
+    public void settings() {
+        size(1200, 720);
+    }
+
+    public void draw(){
+        rect(50,50,50,50);
+    }
+
 
 }
