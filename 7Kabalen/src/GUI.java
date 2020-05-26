de9889ac191822ee009f1e76b7e48c26d297e9a2
mspruc <55182629+mspruc@users.noreply.github.com>
2020-05-26 12:47:24 +0200

    Delete GUI.java

--- 7Kabalen/src/GUI.java
+++
@@ -1,9 +0,0 @@
-public class GUI {
-    /*
-        GUI should contain all graphic displays such as:
-            Buttons
-            Graph display
-            Text information
-            Any visual display.
-     */
-}
